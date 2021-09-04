d98c0ec024bc31a5faabfab634f29864cc0c3204
Sataka <gundama2019@gmail.com>
2021-09-04 21:40:10 +0700

    upload new web food

---
+++ access/JS/main.js
@@ -0,0 +1,14 @@
+var menu = document.querySelector('.icon-menu')
+var nav = document.querySelector('.nav')
+menu.onclick = function() {
+    menu.style.display = 'none'
+    nav.style.display = 'block'
+    Close.style.display = 'block'
+}
+// Css cho close
+var Close = document.querySelector('.icon-close')
+Close.onclick = function() {
+    nav.style.display = 'none'
+    menu.style.display = 'block'
+    Close.style.display = 'none'
+}
